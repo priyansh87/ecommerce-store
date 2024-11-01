e4408882908791603d5b5225fbe5b8f56aa5a950
priyansh87 <priyansh56701@gmail.com>
2024-11-01 20:02:22 +0530

    cloudinary setup

---
+++ backend/lib/cloudinary.js
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from "cloudinary"
+import dotenv from "dotenv" 
+
+dotenv.config() 
+
+    // CLOUDINARY_CLOUD_NAME
+    // CLOUDINARY_API_KEY_SECRET 
+    // CLOUDINARY_API_KEY
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_KEY_SECRET,
+
+})
+
+export default cloudinary ; 
