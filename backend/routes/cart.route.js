67a1ec705bbb73710b0694cfe8ed2f959ac1f8d6
priyansh87 <priyansh56701@gmail.com>
2024-11-02 11:38:28 +0530

    cart routes and controllers -completed

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,17 @@
+import express from "express" 
+import { addtoCart , removeAllFromCart , updateQuantity , getCartProducts} from "../controllers/cart.controllers"
+import { protectRoute } from "../middleware/auth.middleware";
+
+
+const route = express.Router() 
+
+// protected route adds user to the body 
+
+route.get("/" ,protectRoute, getCartProducts ) ; 
+route.post("/" ,protectRoute, addtoCart) ; 
+route.delete("/" , protectRoute , removeAllFromCart) ; 
+route.put("/:id" , protectRoute , updateQuantity) ; 
+
+
+
+export default route ; 
