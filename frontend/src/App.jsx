5c1c01b7b87772175366b4aeab55846f40d18969
priyansh87 <priyansh56701@gmail.com>
2024-11-05 23:28:43 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,7 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
 
 
 function App() {
@@ -8,6 +12,16 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-red-800">
       Hello world!
     </h1>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+
     </>
   )
 }
