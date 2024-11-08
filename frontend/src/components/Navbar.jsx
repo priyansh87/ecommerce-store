868553e8026148a4572f18e7d5f2e681a0718e2d
priyansh87 <priyansh56701@gmail.com>
2024-11-08 12:17:49 +0530

    admin page created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useUserStore } from "../stores/useUserStore";
 function Navbar() {
-    const user = true ; 
-    const isAdmin = true ; 
+    const {user , logout} = useUserStore() ; 
+    const isAdmin = user?.role === "admin" ; 
     const cart = [1,2,3] ;
-    const logout = ()=>{}
+    
   return (
     <>
         <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
