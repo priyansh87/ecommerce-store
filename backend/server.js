fadc266e5440324d6c0de44d6320f67e093333ff
priyansh87 <priyansh56701@gmail.com>
2024-11-10 14:59:54 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use( express.static("public"))
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart" , cartRoutes)
-app.use("/api/coupon" , couponRoutes)
+app.use("/api/coupons" , couponRoutes)
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
